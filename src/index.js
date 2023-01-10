fb2655fbe9327430c066abd10e92c315e2c1a032
Cameron Romo <cromo1225@gmail.com>
2023-01-09 19:16:20 -0700

    Add initial structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import { bugAdded, bugResolved } from './actions';
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed!', store.getState());
+});
+
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugResolved(1));
+
+unsubscribe();
+
+console.log('store.getState() :>> ', store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
