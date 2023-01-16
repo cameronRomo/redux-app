0f94ab7cf0e55e996022fcfb2a9c689520a8d977
Cameron Romo <cromo1225@gmail.com>
2023-01-16 11:52:48 -0700

    Add devtools extension and source mapping

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import reducer from './reducer';
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-const store = createStore(reducer, devTools);
+const store = createStore(reducer, devToolsEnhancer({ trace: true }));
 
 export default store;
