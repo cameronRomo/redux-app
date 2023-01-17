71e77ff16272ccc1d317702bc825723d2b78b8b9
Cameron Romo <cromo1225@gmail.com>
2023-01-17 11:22:25 -0700

    Model how to make async api calls with custom middleware

--- src/index.js
+++ src/index.js
@@ -10,24 +10,13 @@ import { userAdded } from './store/users';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log('Store changed!');
+store.dispatch((dispatch, getState) => {
+  // Call an API
+  // When promise is resolved => dispatch()
+  dispatch({ type: 'bugsRecieved', bugs: [1, 2, 3]});
+  console.log(getState());
+  // If promise is rejected => dispatch()
 });
-
-store.dispatch(userAdded({ name: "User 1"}));
-// store.dispatch(userAdded({ name: "User 2"}));
-// store.dispatch(projectAdded({ name: "Project 1" }));
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1}));
-// store.dispatch(bugResolved({ id: 1 }));
-
-const unresolvedBugs = getUnresolvedBugs(store.getState());
-const userBugs = getBugsByUser(1)(store.getState());
-console.log('userBugs :>> ', userBugs);
-
-console.log('unresolvedBugs :>> ', unresolvedBugs);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
